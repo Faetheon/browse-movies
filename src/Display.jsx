0e0379d7b21b28486b07ec49920b0cb7ba6278bd
unknown <nateohill@gmail.com>
2018-06-26 15:52:14 -0500

    Display currently does nothing but I am trying to have it filter my results.

---
+++ src/Display.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Movie from './Movie.jsx';
+
+class Display extends React.Component {
+	render() {
+		return (
+			<div className="Display">
+				{/*				<div className="movieContainer">
+					{this.getResults.map(result => <Movie key={result.id} result={result} />)}
+				</div>*/}
+			</div>
+		);
+	}
+}
+
+export default Display;
